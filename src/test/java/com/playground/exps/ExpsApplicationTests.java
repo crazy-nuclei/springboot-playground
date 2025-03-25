7cb855bfa640c7f5e6e606a0e774f1673d7468d6
Aayush Agarwal <crazy_nuclei@Aayushs-MacBook-Air.local>
2025-03-25 15:15:06 +0530

    initial commit setup

---
+++ src/test/java/com/playground/exps/ExpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.exps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
